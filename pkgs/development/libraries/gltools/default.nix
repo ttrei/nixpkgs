04677e1a5c75222a703d78e1771501f5409c3dd5
Reinis Taukulis <reinis.taukulis@gmail.com>
2017-12-03 08:41:56 +0200

    bump version

--- pkgs/development/libraries/gltools/default.nix
+++ pkgs/development/libraries/gltools/default.nix
@@ -6,8 +6,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = "/var/tmp/gltools.git";
-    rev = "refs/heads/master";
-    sha256 = "0nyk8nkw27s59yi08g2xvyz1h94g0igm83wq76dv9djb04axrdx4";
+    rev = "f27ce38fd36c6177a5a34fcea44a5e8b46a54b4f";
+    sha256 = "1zi1sa3akc8jcwyn0iyj34h1dpqzcwax27khd0xsgg2mbqlyi4rc";
   };
 
   nativeBuildInputs = [ gcc cmake ];
