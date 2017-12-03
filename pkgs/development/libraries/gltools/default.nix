72c8c04aa238ce515900454de6a8c1209e5c0ae1
Reinis Taukulis <reinis.taukulis@gmail.com>
2017-12-03 17:32:13 +0200

    ssh config updated

--- pkgs/development/libraries/gltools/default.nix
+++ pkgs/development/libraries/gltools/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "0.1";
 
   src = fetchgitPrivate {
-    url = "ssh://reinis@mazais:/home/reinis/projects/gltools.git";
+    url = "ssh://mazais-fetchgit:/home/reinis/projects/gltools.git";
     rev = "e91924a6a0fb7be0901104c629915bae2ed8a62e";
     sha256 = "0bh4q7326842qn2vcm2ifq0qk810rpnsfppblwjgz5ydvp3dsbjc";
   };
